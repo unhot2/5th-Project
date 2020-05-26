4e6291ca31ddca01d0654057dfd6fc1487e2349c
unhot2 <62008631+unhot2@users.noreply.github.com>
2020-05-26 20:14:32 +0900

    api 병합

---
+++ src/main/java/com/team/service/KakaoServiceImpl.java
@@ -0,0 +1,96 @@
+package com.team.service;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class KakaoServiceImpl {
+	private final static String K_CLIENT_ID = "c1a06341cd3bec3417158db98628ca2e";
+	private final static String K_REDIRECT_URI = "http://localhost:8083/sample/kakaoLogin";
+
+	public String getUrl(HttpSession session) {
+		String kakaoUrl = "https://kauth.kakao.com/oauth/authorize?" + "client_id=" + K_CLIENT_ID + "&redirect_uri="
+				+ K_REDIRECT_URI + "&response_type=code";
+		return kakaoUrl;
+	}
+
+	public JsonNode getAccessToken(String autorize_code) {
+		final String RequestUrl = "https://kauth.kakao.com/oauth/token";
+		final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
+		postParams.add(new BasicNameValuePair("client_id", K_CLIENT_ID));
+		postParams.add(new BasicNameValuePair("redirect_uri", K_REDIRECT_URI));
+		postParams.add(new BasicNameValuePair("code", autorize_code));
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
+		JsonNode returnNode = null;
+		try {
+			post.setEntity(new UrlEncodedFormEntity(postParams));
+			final HttpResponse response = client.execute(post);
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return returnNode;
+	}
+
+	public JsonNode getKakaoUserInfo(JsonNode accessToken) {
+		final String RequestUrl = "https://kapi.kakao.com/v2/user/me";
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
+		post.addHeader("Authorization", "Bearer " + accessToken);
+		JsonNode returnNode = null;
+		try {
+			final HttpResponse response = client.execute(post);
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return returnNode;
+	}
+
+	public JsonNode Logout(String autorize_code) {
+		final String RequestUrl = "https://kapi.kakao.com/v2/user/logout";
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
+		post.addHeader("Authorization", "Bearer " + autorize_code);
+		JsonNode returnNode = null;
+		try {
+			final HttpResponse response = client.execute(post);
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return returnNode;
+	}
+}
--- src/main/webapp/WEB-INF/views/login/login.jsp
+++ src/main/webapp/WEB-INF/views/login/login.jsp
@@ -2,14 +2,33 @@
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <!DOCTYPE html>
+
 <jsp:include page="../include/header.jsp" />
+
 <section class="loginSection">
-	
-		<form action="loginChk" method="post">
-			아이디 : <input type="text" name="userId"><br> 비밀번호 : <input
-				type="text" name="userPwd"> <input type="submit" value="로그인">
-		</form>
-		<button type="button" onclick="location.href='memberShip'">회원가입</button>
-	
+	<form action="loginChk" method="post">
+		<table border="1">
+			<tr>
+				<th>아이디</th>
+				<th><input type="text" name="userId"></th>
+			</tr>
+			<tr>
+				<th>비밀번호</th>
+				<th><input type="text" name="userPwd"></th>
+			</tr>
+			<tr>
+				<th colspan="2"><a
+					href="${kakaoUrl }">
+						<img src="resources/img/kakao_login_btn_medium_narrow.png">
+				</a></th>
+			</tr>
+			<tr>
+				<th><input type="submit" value="로그인"></th>
+				<th><button type="button" onclick="location.href='memberShip'">회원가입</button></th>
+			</tr>
+		</table>
+
+	</form>
+
 </section>
 <jsp:include page="../include/footer.jsp" />
