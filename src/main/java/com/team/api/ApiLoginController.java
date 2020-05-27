e28a8d6b9991153f2d5dd5215c02f88d5877fb66
aaaa.aaaa <aaaa@aaa.aaa>
2020-05-27 16:20:55 +0900

    5.27 네이버 미완성

--- src/main/java/com/team/api/ApiLoginController.java
+++ src/main/java/com/team/api/ApiLoginController.java
@@ -24,10 +24,11 @@ import com.team.service.KakaoServiceImpl;
 public class ApiLoginController {
 	@Autowired
 	KakaoServiceImpl kakao;	
-		private NaverLoginBO naverLoginBO;
-		private String apiResult = null;
-		@Autowired
-		private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
+	private NaverLoginBO naverLoginBO;
+	private String apiResult = null;
+	
+	@Autowired
+	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
 			this.naverLoginBO = naverLoginBO;
 		}
 
@@ -53,18 +54,15 @@ public class ApiLoginController {
 			oauthToken = naverLoginBO.getAccessToken(session, code, state);
 			// 1. 로그인 사용자 정보를 읽어온다.
 			apiResult = naverLoginBO.getUserProfile(oauthToken); // String형식의 json데이터
-			/**
-			 * apiResult json 구조 {"resultcode":"00", "message":"success",
-			 * "response":{"id":"33666449","nickname":"shinn****","age":"20-29","gender":"M","email":"sh@naver.com","name":"\uc2e0\ubc94\ud638"}}
-			 **/
 			// 2. String형식인 apiResult를 json형태로 바꿈
 			JSONParser parser = new JSONParser();
 			Object obj = parser.parse(apiResult);
 			JSONObject jsonObj = (JSONObject) obj;
 			// 3. 데이터 파싱
-			// Top레벨 단계 _response 파싱
+			
 			JSONObject response_obj = (JSONObject) jsonObj.get("response");
-			// response의 nickname값 파싱
+			System.out.println(response_obj);
+			
 			String nickname = (String) response_obj.get("nickname");
 			System.out.println(nickname);
 			// 4.파싱 닉네임 세션으로 저장
